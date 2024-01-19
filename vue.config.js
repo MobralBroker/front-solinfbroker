4793775d237f1f7fd8509d8b2ca80be0c0c17973
jvlr95 <jvitor.lr@hotmail.com>
2024-01-19 00:57:04 -0300

    Configuração correta para expor porta do frontend -- 8085

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
 
-})
+  devServer: {
+    port: 8085, // Defina a porta desejada
+  },
 
-module.exports = {
   configureWebpack: {
     watchOptions: {
       poll: 1000, // Intervalo de polling em milissegundos
     },
   },
-};
+});
 
